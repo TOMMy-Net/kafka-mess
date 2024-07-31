5a22c84b316bc6c1004847b6defce9d056309c74
Timofey1234567890 <74640949+Timofey1234567890@users.noreply.github.com>
2024-07-31 23:37:38 +0300

    add stats

---
+++ app/internal/handlers/api/stats.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"github.com/TOMMy-Net/kafka-mess/internal/models"
+	"github.com/TOMMy-Net/kafka-mess/internal/render"
+	"github.com/gofiber/fiber/v2"
+)
+
+func (a *ApiHandlers) GetStatsMessages(c *fiber.Ctx) error {
+	var stats models.MessageStats
+	count, err := a.DB.CountMessages(c.Context())
+	if err != nil {
+		return render.SendServerError(c, err)
+	}
+
+	unSend, err := a.DB.UnSendMessages(c.Context())
+	if err != nil {
+		return render.SendServerError(c, err)
+	}
+
+	stats.TotalMessages = count
+	stats.UnSendMessages = len(unSend)
+	stats.SendMessages = stats.TotalMessages - stats.UnSendMessages
+
+	return c.Status(fiber.StatusOK).JSON(stats)
+}
